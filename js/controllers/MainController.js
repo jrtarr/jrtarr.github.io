e8b0a766e58d34341a5405bef789f0003c7ffc55
James Tarr <jrtarr@purdue.edu>
2017-06-22 23:14:55 -0400

    Fixes on Project layout, including AngularJS fixes and CSS tweaks. Added footer w/ Angular functionality. Added logo and page header.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,20 +1,37 @@
 app.controller('MainController', ['$scope', function($scope){
 		$scope.b45s = {
-			name: 'Website - The B45s',
+			name: 'Freelance - The B45s',
 			startDate: new Date(2016, 04, 21),
 			endDate: 'Ongoing',
-			description: 'Designed and maintained website for Fort Wayne rock band, the B45s.',
+			description: 'Designed, created, and maintained website for Fort Wayne rock band, the B45s.',
 			skillsUsed: 'HTML, CSS, Bootstrap',
 			link: 'http://theb45s.com',
 			img: 'images/Projects/b45slogo.svg'
 		};
 		$scope.execuwell = {
-			name: 'Website - Execuwell',
+			name: 'Freelance - Execuwell',
 			startDate: new Date(2017, 02, 01),
 			endDate: 'Ongoing',
-			description: 'Designed and maintained website for Indianapolis-area wellness consultancy, Execuwell.',
+			description: 'Designed, created, and maintained website for Indianapolis-area wellness consultancy, Execuwell.',
 			skillsUsed: 'HTML, CSS, Javascript, JQuery, Bootstrap',
 			link: 'http://execuwell.com',
 			img: 'images/Projects/execuwellLogo.png'
 		};
+		$scope.footerIcons = [
+		{
+			name: 'twitter',
+			iFrame: 'fa fa-twitter',
+			link: 'https://twitter.com/jrt1025'
+		},
+		{
+			name: 'linkedIn',
+			iFrame: 'fa fa-linkedin-square',
+			link: 'https://www.linkedin.com/in/james-tarr-34849197/'
+		},
+		{
+			name: 'github',
+			iFrame: 'fa fa-github',
+			link: 'https://github.com/jrtarr'
+		}
+		]
 	}]);

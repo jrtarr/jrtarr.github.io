f90265a2fcbd755cbdc51bd35c18c9473c0d89da
Tarr <jtarr@txt.textron.com>
2017-06-27 13:43:35 -0500

    Fixed project filtering, added more projects and graphics

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,25 +1,69 @@
 app.controller('MainController', ['$scope', function($scope){
 		$scope.displayCat = 'webdev';
+		var date = new Date();
+		date = date.toString();
+		console.log(date);
+		console.log(new Date(2017, 02, 01));
 		$scope.projects = [
 		{
 			category: 'webdev',
-			name: 'Freelance - The B45s',
+			name: 'The B45s',
 			startDate: new Date(2016, 04, 21),
-			endDate: 'Ongoing',
+			endDate: new Date(),
 			description: 'Designed, created, and maintained website for Fort Wayne rock band, the B45s.',
 			skillsUsed: 'HTML, CSS, Bootstrap',
 			link: 'http://theb45s.com',
-			img: 'images/Projects/b45slogo.svg'
+			img: 'images/Projects/Web/b45slogo.svg'
 		},
 		{
-			category: 'cad',
-			name: 'Freelance - Execuwell',
+			category: 'webdev',
+			name: 'Execuwell',
 			startDate: new Date(2017, 02, 01),
-			endDate: 'Ongoing',
+			endDate: new Date(),
 			description: 'Designed, created, and maintained website for Indianapolis-area wellness consultancy, Execuwell.',
 			skillsUsed: 'HTML, CSS, Javascript, JQuery, Bootstrap',
 			link: 'http://execuwell.com',
-			img: 'images/Projects/execuwellLogo.png'
+			img: 'images/Projects/Web/execuwellLogo.png'
+		},
+		{
+			category: 'graphics',
+			name: 'My Apollo Debut Poster',
+			startDate: new Date(2016, 05, 15),
+			endDate: new Date(2016, 05, 17),
+			description: 'Commissioned to create poster for Fort Wayne band My Apollo\'s debut performance.',
+			skillsUsed: 'Adobe Illustrator, Adobe Photoshop',
+			link: 'https://www.facebook.com/myapolloofficial/photos/a.1785673638331860.1073741829.1781663952066162/1785672838331940/?type=3&theater',
+			img: 'images/Projects/GraphicDesign/StarBack.png'
+		},
+		{
+			category: 'graphics',
+			name: 'My Apollo @ CS3 Poster',
+			startDate: new Date(2016, 08, 11),
+			endDate: new Date(2016, 08, 14),
+			description: 'Commissioned to create poster for Fort Wayne band My Apollo.',
+			skillsUsed: 'Adobe Illustrator, Adobe Photoshop',
+			link: 'https://www.facebook.com/myapolloofficial/photos/a.1781664765399414.1073741827.1781663952066162/1822640307968526/?type=3&theater',
+			img: 'images/Projects/GraphicDesign/PopArt.png'
+		},
+		{
+			category: 'cad',
+			name: 'PLACEHOLDER CATIA PROJECT',
+			startDate: new Date(2016, 08, 15),
+			endDate: new Date(2016, 09, 17),
+			description: 'Make a fake CAD project in CATIA to prove this space works',
+			skillsUsed: 'Dassault CATIA v24',
+			link: '#',
+			img: 'images/Projects/CAD/PlaceholderCATIA.gif'
+		},
+		{
+			category: 'cad',
+			name: 'PLACEHOLDER NX PROJECT',
+			startDate: new Date(2016, 010, 11),
+			endDate: new Date(2016, 11, 14),
+			description: 'I made a fake project in NX9 to prove this space loads properly. Hopefully a real one appears soon.',
+			skillsUsed: 'NX9',
+			link: '#',
+			img: 'images/Projects/CAD/PlaceholderNX.jpg'
 		}
 		];
 
@@ -54,6 +98,7 @@ app.controller('MainController', ['$scope', function($scope){
 				return true;
 			}
 		};
+
 		$scope.catSwap = function(newCat){
 			if(newCat == "web"){
 				$scope.displayCat =	"webdev";
@@ -65,4 +110,6 @@ app.controller('MainController', ['$scope', function($scope){
 				$scope.displayCat = "graphics";
 			}
 		};
+
+		$scope.$watch
 	}]);

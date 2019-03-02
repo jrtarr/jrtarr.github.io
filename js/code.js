00638d8134d9ed60e38630dca3f25823f648b1b8
James Tarr <jrt1025@gmail.com>
2019-03-02 15:11:29 -0500

    Add Client Divs

--- js/code.js
+++
@@ -1,7 +0,0 @@
-function spinThisBitch() {
-    document.getElementById("logo").classList.add("spin");
-    window.setTimeout(() => {  
-        document.getElementById("logo").classList.remove("spin");
-    }, 1000);
-    console.log("I'm Working Kinda");
-}

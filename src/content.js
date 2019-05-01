6d1c862d87af26632ef70dcd52f95822cbe076ff
James Tarr <jrt1025@gmail.com>
2019-05-01 16:58:52 -0400

    Sections Built, Added theme toggle

--- src/content.js
+++ src/content.js
@@ -1,7 +1,7 @@
 //Home of Content to be Loaded
 export default content =  {
     textContent: {
-        aboutMe: ['Purdue University graduate with a triple major in Computer Graphics Technology, Web Development and Design, and Virtual Product Integration. My passion for web and graphic design spans broadly from creating both raster and vector graphics, to creating functional and aesthetically pleasing websites, and even creating CAD products using CATIA and NX9.','<br/>I\'ve worked on a wide variety projects including site design, web app support, and graphic design. I\'ve interned with <a href="https://www.textron.com/">Textron Inc.</a> in their IT leadership development program working on backend web development, and most recently worked at <a href="https://www.epic.com/">Epic Systems</a> to support MyChart - a .NET based web application that patients use to view their medical information.']
+        aboutMe: ['I\'m James, a Purdue University graduate with a triple major in Computer Graphics Technology, Web Development and Design, and Virtual Product Integration. My passion for web and graphic design spans broadly from creating both raster and vector graphics, to creating functional and aesthetically pleasing websites, and even creating CAD products using CATIA and NX9.','<br/>I\'ve worked on a wide variety projects including site design, web app support, and graphic design. I\'ve interned with <a href="https://www.textron.com/">Textron Inc.</a> in their IT leadership development program working on backend web development, and most recently worked at <a href="https://www.epic.com/">Epic Systems</a> to support MyChart - a .NET based web application that patients use to view their medical information.']
     },
     social: [
         {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
+//Main page controllers live here
 import { renderText,renderFooter } from "./views";
 
-//Main page controllers live here
-
+// const nav = document.querySelector('nav')
+// const navButton = document.querySelector('#nav-menu')
+// const navList = document.querySelector('#nav-list')
+const themeButtons = document.querySelectorAll('.theme-button')
+let theme = 'Web'
 renderText()
 renderFooter()
+
+themeButtons.forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        themeButtons.forEach((button)=>{
+            button.classList.toggle('active')
+        })
+        theme = e.target.value
+        console.log(theme)
+    })
+})
+
+// navButton.addEventListener('click',(e)=>{
+//     console.log('Clicked!')
+//     navButton.classList.toggle('expanded')
+//     nav.classList.toggle('expanded')
+//     //navList.classList.toggle('hidden')
+// })
